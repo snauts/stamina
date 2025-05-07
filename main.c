6ea4d4507e360214f952bbd18d5ca99c5b76b4ce
Artūrs Grebstelis <arturs.grebstelis@gmail.com>
2025-05-07 11:04:38 +0300

    add memory manipulation routines

--- main.c
+++ main.c
@@ -12,6 +12,30 @@ typedef unsigned short word;
 #define SETUP_STACK()	__asm__("ld sp, #0xfdfc")
 #endif
 
+static void out_fe(byte data) {
+    __asm__("out (#0xfe), a"); data;
+}
+
+static void __sdcc_call_hl(void) __naked {
+    __asm__("jp (hl)");
+}
+
+static void memset(byte *ptr, byte data, word len) {
+    while (len-- > 0) { *ptr++ = data; }
+}
+
+static void memcpy(byte *dst, const byte *src, word len) {
+    while (len-- > 0) { *dst++ = *src++; }
+}
+
+static void clear_screen(void) {
+#if defined(ZXS)
+    memset((byte *) 0x5800, 0x00, 0x300);
+    memset((byte *) 0x4000, 0x00, 0x1800);
+    out_fe(0);
+#endif
+}
+
 static void decompress(byte *dst, const byte *src) {
     while (*src) {
         byte n = *src & 0x7f;
@@ -32,5 +56,6 @@ static void decompress(byte *dst, const byte *src) {
 
 void reset(void) {
     SETUP_STACK();
+    clear_screen();
     for (;;) { }
 }
