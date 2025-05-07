1c74fed43c8aa78b75ed7e0666207bb04f1e9cbe
Artūrs Grebstelis <arturs.grebstelis@gmail.com>
2025-05-07 12:36:42 +0300

    add text output routines

--- main.c
+++ main.c
@@ -15,7 +15,7 @@ typedef unsigned short word;
 #define BYTE(addr)	(* (volatile byte *) (addr))
 #define WORD(addr)	(* (volatile word *) (addr))
 #define SIZE(array)	(sizeof(array) / sizeof(*(array)))
-#define PTR(addr)	((void *) (addr))
+#define PTR(addr)	((byte *) (addr))
 
 static byte *map_y[192];
 
@@ -23,7 +23,8 @@ static byte *map_y[192];
 #define SETUP_STACK()	__asm__("ld sp, #0xfdfc")
 #define SCREEN(x)	PTR(0x4000 + (x))
 #define COLOUR(x)	PTR(0x5800 + (x))
-#define STAGING_AREA	COLOUR(0x300)
+#define STAGING_AREA	PTR(0x5b00)
+#define FONT_ADDRESS	PTR(0x3c00)
 #endif
 
 static void out_fe(byte data) {
@@ -59,6 +60,81 @@ static void clear_screen(void) {
 #endif
 }
 
+static void put_char(char symbol, byte x, byte y) {
+    byte shift = x & 7;
+    byte offset = x >> 3;
+    byte *addr = FONT_ADDRESS + (symbol << 3);
+    for (byte i = 0; i < 8; i++) {
+	byte data = *addr++;
+	byte *ptr = map_y[y + i] + offset;
+	ptr[0] |= (data >> shift);
+	ptr[1] |= (data << (8 - shift));
+    }
+}
+
+static byte char_mask(char symbol) {
+    byte mask = 0;
+    byte *addr = FONT_ADDRESS + (symbol << 3);
+    for (byte i = 0; i < 8; i++) {
+	mask |= *addr++;
+    }
+    return mask;
+}
+
+static byte leading(char symbol) {
+    byte i;
+    byte mask = char_mask(symbol);
+    for (i = 0; i < 8; i++) {
+	if (mask & 0x80) goto done;
+	mask = mask << 1;
+    }
+  done:
+    return i - 1;
+}
+
+static byte trailing(char symbol) {
+    byte i;
+    byte mask = char_mask(symbol);
+    for (i = 0; i < 8; i++) {
+	if (mask & 1) goto done;
+	mask = mask >> 1;
+    }
+  done:
+    return 8 - i;
+}
+
+static void put_str(const char *msg, byte x, byte y) {
+    while (*msg != 0) {
+	char symbol = *(msg++);
+	if (symbol == ' ') {
+	    x = x + 4;
+	}
+	else {
+	    byte lead = leading(symbol);
+	    if (lead <= x) x -= lead;
+	    put_char(symbol, x, y);
+	    x += trailing(symbol);
+	}
+    }
+}
+
+static byte str_len(const char *msg) {
+    byte len = 0;
+    while (*msg != 0) {
+	char symbol = *(msg++);
+	len += symbol == ' ' ? 4 : trailing(symbol) - leading(symbol);
+    }
+    return len;
+}
+
+static byte str_offset(const char *msg, byte from) {
+    return from - (str_len(msg) >> 1);
+}
+
+static void center_msg(const char *msg, byte y) {
+    put_str(msg, str_offset(msg, 128), y);
+}
+
 static void decompress(byte *dst, const byte *src) {
     while (*src) {
         byte n = *src & 0x7f;
@@ -89,6 +165,7 @@ static void show_block(const void *src, byte y, byte n) {
 
 static void show_title(void) {
     show_block(title, 64, 64);
+    center_msg("Stamina", 116);
 }
 
 void reset(void) {
