ee889d0cb40bb63d1457ed1a481449b95e9de96c
Artūrs Grebstelis <arturs.grebstelis@gmail.com>
2025-05-12 15:58:33 +0300

    replenish full stamina during rest

--- main.c
+++ main.c
@@ -487,7 +487,7 @@ static void roll_richard(int8 delta) {
 
 static void rest_richard(void) {
     update_image(&player, TILE(RESTED));
-    replenish_stamina(12);
+    replenish_stamina(FULL_STAMINA);
 }
 
 static void move_richard(void) {
